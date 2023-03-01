48d2071123c838a77d67e2cb7109847d9146dd80
Celia Merel <celia.merel@etu.minesparis.psl.eu>
2023-03-01 15:32:31 +0100

    update 1

--- code.js
+++ code.js
@@ -3,8 +3,34 @@ document.addEventListener("DOMContentLoaded", () => {
   initialCleanup();
 
   // Hey! Pssst! In here ...
-});
 
+
+  document.getElementById("btn-add-line").addEventListener("click",
+    () => {
+      console.log("clique ici !");
+      for (let i=0;i<10;i++) {
+        my_div = document.createElement('div');
+        grid.appendChild(my_div);
+      }
+    })
+
+  document.getElementById("grid").addEventListener("click",()=>{
+    for (square of grid.children){
+      square.addEventListener("click",(event)=>{
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        event.target.style.backgroundColor = "#"+ randomColor;
+      })
+    }
+  })
+
+  document.getElementById("grid").addEventListener("mouseover",()=>{
+    for (square of grid.children){
+      square.addEventListener("mouseover",(event)=>{
+        event.target.style.backgroundColor = "purple";
+      })
+    }
+  })
+});
 /**
  * Cleans up the document so that the exercise is easier.
  *
@@ -22,3 +48,6 @@ function initialCleanup() {
     node.remove();
   }
 }
+
+
+
